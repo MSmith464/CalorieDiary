4cf4c1f06b04893ac16cdea66ee20687fbc6d1af
Morgan <jmorgansmith88@gmail.com>
2020-08-18 22:07:13 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,30 +1,48 @@
 console.log("Hello World!");
 
+
 const apiName = 'API signup';
 const apiID = '2d1ec81e';
 const apiKey = '836d96fcf21461eaa22cd298d896dd07'
 
 // calorie controller
+function calculateCalories() {
+
+}
 
 //food api
 async function getFood(food) {
-    const result = await
-        fetch (`https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=${apiID}&app_key=${apiKey}`);
-    const data = await result.json();
-    // console.log(data);
-    console.log(data.hints[0].food.nutrients.ENERC_KCAL);
+    let cals;
+
+    try {
+        const result = await
+            fetch (`https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=${apiID}&app_key=${apiKey}`);
+
+        //parse the json data    
+        const data = await result.json();
+        console.log(data.hints[0].food.nutrients.ENERC_KCAL);
+        cals = (data.hints[0].food.nutrients.ENERC_KCAL);
+
+        cals = cals.toFixed(2);
+        return cals;
+    }catch(error){
+        console.log(error);
+    }
 };
 
 //UI Controller
 let DOMstrings = {
     foodItem: '.food__item',
-    addButton: '.add__btn'
+    addButton: '.add__btn',
+    numItems: '.num__items'
 }
 
 function getInput() {
-    const item = document.querySelector('.food__item').value;
-    console.log(item);
-    return item;
+    foodData = {
+        foodItem: document.querySelector(DOMstrings.foodItem).value,
+        amount: parseInt(document.querySelector(DOMstrings.numItems).value)
+    }
+    return (foodData);
 }
 
 
@@ -41,14 +59,21 @@ let setupEventListeners = function() {
 }
 
 function addItem() {
-    let input, newItem;
-
-    input = getInput();
-
-    getFood(input);
+    let input, calories, newItem;
 
 
     // 1. Get new item entry
+    input = getInput();
+
+    if (input.foodItem !== "" && input.amount > 0){
+        getFood(input.foodItem).then(calories =>
+            console.log(`The amount of calories in your ${input.foodItem} is ${calories}`));
+    }else {
+        alert("Please enter a food item and an amount.")
+    }
+
+
+    
 
     // 2. Add item to calorie controller
 
